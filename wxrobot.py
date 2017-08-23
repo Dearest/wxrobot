f6f6be7e5129cbc30232fed5aca21f55eaa06a7f
Dearest <hanchen@126.com>
2017-08-23 16:10:09 +0800

    Update wxrobot.py
    删掉无用代码

--- wxrobot.py
+++ wxrobot.py
@@ -8,26 +8,15 @@ from meizitu import *
 # 初始化机器人，扫码登陆
 bot = Bot(console_qr=1)
 schoolmate = bot.groups().search('君子藏器于身待时而动')[0]
-boss = bot.friends().search('Dearest')[0]
 
-@bot.register( [schoolmate, boss], TEXT)
-def cook_book(msg):
-    if re.match(re.compile(r'^#[晚餐|晚饭|菜谱]'), msg.text):
-        print(msg.text)
-        ParseHtmlToImg.parse(CookBook.random_path()[0])
-        msg.reply_image('cook_book.jpg')
-    elif re.match(re.compile(r'^#[妹子|美女]'), msg.text):
+@bot.register(schoolmate, TEXT)
+def girl(msg):
+    if re.match(re.compile(r'^#[妹美女奶]'), msg.text):
         print(msg.text)
         path = MeiZiTu.random_pic_path()
         print(path)
         for x in path:
             schoolmate.send_image(x)
-    elif re.match(re.compile(r'^#[张杰|杰少]'), msg.text):
-        return '傻逼'
-    elif re.match(re.compile(r'^#[曾]'), msg.text):
-        return '国服第一李索斯'
-    elif re.match(re.compile(r'^#[罡]'), msg.text):
-        return '国服第一辅助'
     else:
         return
 bot.join()
